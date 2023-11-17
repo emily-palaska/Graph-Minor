358df06ad848ab23eb827b364364a8993e9069fd
Emily Palaska <103355365+emily-palaska@users.noreply.github.com>
2023-11-17 17:10:52 +0200

    Add files via upload

---
+++ matrix_generator.m
@@ -0,0 +1,32 @@
+clc;
+n = 4000;
+m = 0.01 * n ^ 2;
+line = "%%MatrixMarket matrix coordinate real unsymmetric";
+output = line + newline + num2str(n) + ' ' + num2str(n) + ' ' + num2str(m) + newline;
+
+coor = zeros(m, 2);
+val = zeros(m, 1);
+U = randperm(n^2, m);
+
+for k = 1:m
+    i = floor(U(k) / n) + 1;
+    j = mod(U(k), n) + 1;
+    coor(k, 1) = i;
+    coor(k, 2) = j;
+    val(k, 1) = unifrnd(1, 100);
+end
+
+coor = sortrows(coor, 1);
+
+for k = 1:m
+    output = output + num2str(coor(k,1)) + ' ';
+    output = output + num2str(coor(k,2)) + ' ';
+    output = output + num2str(val(k,1)) + newline;
+end
+
+
+writelines(output, '9n4000.txt');
+
+
+
+
